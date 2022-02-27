faa626928f75de0afdbbe623ab0b94230285ee64
LoveToSpooch <love2spooch@mail.ru>
2022-02-27 11:45:30 +0300

    Add ShopItemActivity - second activity

---
+++ app/src/main/java/cheers/lovetospooch/shoppinglist/presentation/ShopItemViewModel.kt
@@ -0,0 +1,113 @@
+package cheers.lovetospooch.shoppinglist.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import cheers.lovetospooch.shoppinglist.data.ShopListRepositoryImpl
+import cheers.lovetospooch.shoppinglist.domain.*
+import java.lang.Exception
+
+class ShopItemViewModel: ViewModel() {
+
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopItemUseCase = GetItemIdUseCase(repository)
+    private val addToShopItemUseCase = AddToShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    private val _errorInputName = MutableLiveData<Boolean>()
+    val errorInputName: LiveData<Boolean>
+        get() = _errorInputName
+
+    private val _errorInputCount = MutableLiveData<Boolean>()
+    val errorInputCount: LiveData<Boolean>
+    get() = _errorInputCount
+
+    private val _shopItem = MutableLiveData<ShopItem>()
+    val shopItem: LiveData<ShopItem>
+        get() = _shopItem
+
+    private val _shouldCloseScreen = MutableLiveData<Unit>()
+    val shouldCloseScreen: LiveData<Unit>
+    get() = _shouldCloseScreen
+
+
+    fun getShopItem(shopItemId : Int) {
+        val item = getShopItemUseCase.getIdItem(shopItemId)
+        _shopItem.value = item
+    }
+
+
+
+    // Пусть данный метод принимает не shopItem, а те данные которые были считаны из полей ввода
+    fun addShopItem(inputName: String?, inputCount: String?) {
+        val name = parseName(inputName)
+        val count = parseCount(inputCount)
+        val fieldsValid = validateInput(name, count)
+        if (fieldsValid) {
+            val shopItem = ShopItem(name, count, true)
+            addToShopItemUseCase.addToShopItem(shopItem)
+            finishWork()
+        }
+
+    }
+
+    fun editShopItem(inputName: String?, inputCount: String?) {
+        val name = parseName(inputName)
+        val count = parseCount(inputCount)
+        val fieldsValid = validateInput(name, count)
+        if (fieldsValid) {
+
+            _shopItem.value?.let {
+                val item = it.copy(name = name, count = count)
+                editShopItemUseCase.editShopItem(item)
+                finishWork()
+            }
+
+        }
+    }
+
+    private fun parseName(inputName: String?) : String {
+        // если inputName null, то вернем пустую строку
+        // если не  null, то вернем String и обрежем все пробелы
+        return inputName?.trim() ?: ""
+    }
+
+    private fun parseCount(inputCount: String?): Int {
+        return try {
+            inputCount?.trim()?.toInt() ?: 0
+        } catch (e: Exception) {
+            0
+        }
+    }
+
+    private fun validateInput(name: String, count: Int): Boolean {
+        var result = true
+
+        if(name.isBlank()) {
+            _errorInputName.value = true
+            result = false
+        }
+        if (count <= 0) {
+            _errorInputCount.value = true
+            result = false
+        }
+        return result
+    }
+
+    fun resetErrorInputName() {
+        _errorInputName.value = false
+
+    }
+
+    fun resetErrorInputCount() {
+        _errorInputCount.value = false
+
+    }
+
+    private fun finishWork() {
+        _shouldCloseScreen.value = Unit
+    }
+
+
+}
